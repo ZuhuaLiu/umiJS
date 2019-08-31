f930397c55d39e2a8dd1d40b292ee732133e617d
ZuhuaLiu <liuzuhua9510@163.com>
2019-08-31 17:17:40 +0800

    umi

---
+++ umi/pages/.umi/router.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Router as DefaultRouter, Route, Switch } from 'react-router-dom';
+import dynamic from 'umi/dynamic';
+import renderRoutes from 'umi/lib/renderRoutes';
+import history from '@tmp/history';
+
+const Router = DefaultRouter;
+
+const routes = [
+  {
+    path: '/admin/users',
+    exact: true,
+    component: require('../admin/users.js').default,
+  },
+  {
+    path: '/',
+    exact: true,
+    component: require('../index.js').default,
+  },
+  {
+    path: '/users',
+    exact: true,
+    component: require('../users.js').default,
+  },
+  {
+    path: '/base',
+    exact: false,
+    component: require('../base/_layout.js').default,
+    routes: [
+      {
+        component: () =>
+          React.createElement(
+            require('D:/Develop/nodejs/node_global/node_modules/umi/node_modules/umi-build-dev/lib/plugins/404/NotFound.js')
+              .default,
+            { pagesPath: 'pages', hasRoutesInConfig: false },
+          ),
+      },
+    ],
+  },
+  {
+    component: () =>
+      React.createElement(
+        require('D:/Develop/nodejs/node_global/node_modules/umi/node_modules/umi-build-dev/lib/plugins/404/NotFound.js')
+          .default,
+        { pagesPath: 'pages', hasRoutesInConfig: false },
+      ),
+  },
+];
+window.g_routes = routes;
+const plugins = require('umi/_runtimePlugin');
+plugins.applyForEach('patchRoutes', { initialValue: routes });
+
+export { routes };
+
+export default class RouterWrapper extends React.Component {
+  unListen = () => {};
+
+  constructor(props) {
+    super(props);
+
+    // route change handler
+    function routeChangeHandler(location, action) {
+      plugins.applyForEach('onRouteChange', {
+        initialValue: {
+          routes,
+          location,
+          action,
+        },
+      });
+    }
+    this.unListen = history.listen(routeChangeHandler);
+    routeChangeHandler(history.location);
+  }
+
+  componentWillUnmount() {
+    this.unListen();
+  }
+
+  render() {
+    const props = this.props || {};
+    return <Router history={history}>{renderRoutes(routes, props)}</Router>;
+  }
+}
